84d8f6ec5aeedc22ec601bb83a90857d38548961
heybluguy <heybluguy@yahoo.com>
2018-01-02 20:28:38 -0700

    initial commit

---
+++ spec/features/ideas/user_can_create_an_idea_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'as a user' do
+  describe 'when im in idea box' do
+   scenario 'i can create an idea' do
+
+   end
+  end
+end
+
+# An idea can be created by a user.
